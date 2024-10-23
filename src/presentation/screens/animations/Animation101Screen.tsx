63500cffc9f6fe5eccd6da1879d496d88979b771
salinas182 <salinas.182@hotmail.com>
2024-10-23 12:30:26 +0200

    extracted animation code to a custom hook

--- src/presentation/screens/animations/Animation101Screen.tsx
+++ src/presentation/screens/animations/Animation101Screen.tsx
@@ -1,33 +1,10 @@
-import React, { useRef } from 'react';
+import React from 'react';
 import {Animated, Easing, Pressable, StyleSheet, Text, View} from 'react-native';
 import { colors } from '../../../config/theme/theme';
+import { useAnimation } from '../../hooks/useAnimation';
 
 export const Animation101Screen = () => {
-  const animatedOpacity = useRef(new Animated.Value(0)).current;
-  const animatedTop = useRef(new Animated.Value(-100)).current;
-
-  const fadeIn = () => {
-    Animated.timing(animatedTop, {
-      toValue: 0,
-      duration: 700,
-      useNativeDriver: true,
-      easing: Easing.bounce/* .elastic(1) */,
-    }).start();
-
-    Animated.timing(animatedOpacity, {
-      toValue: 1,
-      duration: 300,
-      useNativeDriver: true,
-    }).start();
-  };
-
-  const fadeOut = () => {
-    Animated.timing(animatedOpacity, {
-      toValue: 0,
-      duration: 300,
-      useNativeDriver: true,
-    }).start(() => animatedTop.resetAnimation());
-  };
+  const { fadeIn, fadeOut, animatedOpacity, animatedTop, startMovingTopPosition } = useAnimation();
 
   return (
     <View style={styles.container}>
@@ -41,11 +18,14 @@ export const Animation101Screen = () => {
         },
       ]} />
 
-      <Pressable onPress={() => fadeIn()} style={{marginTop: 10}}>
+      <Pressable onPress={() => {
+        fadeIn({});
+        startMovingTopPosition({ initialPosition: -100, easing: Easing.elastic(1), duration: 750 });
+      }} style={{marginTop: 10}}>
         <Text>Fade In</Text>
       </Pressable>
 
-      <Pressable onPress={fadeOut} style={{marginTop: 10}}>
+      <Pressable onPress={() => fadeOut({})} style={{marginTop: 10}}>
         <Text>Fade Out</Text>
       </Pressable>
     </View>
