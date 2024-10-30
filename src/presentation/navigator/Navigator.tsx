8c42c7dee517405e798e86eae2f7bb4034ec1d2e
salinas182 <salinas.182@hotmail.com>
2024-10-30 19:01:58 +0100

    added sectionList and pullToRefresh components

--- src/presentation/navigator/Navigator.tsx
+++ src/presentation/navigator/Navigator.tsx
@@ -6,6 +6,8 @@ import { Animation102Screen } from '../screens/animations/Animation102Screen';
 import { SwitchScreen } from '../screens/switches/SwitchScreen';
 import { AlertScreen } from '../screens/alerts/AlertScreen';
 import { TextInputScreen } from '../screens/inputs/TextInputScreen';
+import { PullToRefreshScreen } from '../screens/ui/PullToRefreshScreen';
+import { CustomSectionListScreen } from '../screens/ui/CustomSectionListScreen';
 
 const Stack = createStackNavigator();
 
@@ -22,6 +24,9 @@ export const Navigator = () => {
       <Stack.Screen name="SwitchScreen" component={SwitchScreen} />
       <Stack.Screen name="AlertScreen" component={AlertScreen} />
       <Stack.Screen name="TextInputScreen" component={TextInputScreen} />
+
+      <Stack.Screen name="PullToRefreshScreen" component={PullToRefreshScreen} />
+      <Stack.Screen name="CustomSectionListScreen" component={CustomSectionListScreen} />
     </Stack.Navigator>
   );
 };
---
+++ src/presentation/screens/ui/PullToRefreshScreen.tsx
@@ -0,0 +1,33 @@
+import React, { useState } from 'react';
+import { Title } from '../../components/ui/Title';
+import { RefreshControl, ScrollView } from 'react-native';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
+import { colors, globalStyles } from '../../../config/theme/theme';
+
+export const PullToRefreshScreen = () => {
+  const [isRefreshing, setIsRefreshing] = useState(false);
+  const { top } = useSafeAreaInsets();
+  const onRefresh = () => {
+    setIsRefreshing(true);
+
+    setTimeout(() => {
+      setIsRefreshing(false);
+    }, 3000);
+  };
+
+  return (
+    <ScrollView
+      refreshControl={
+        <RefreshControl
+          refreshing={isRefreshing}
+          progressViewOffset={top}
+          onRefresh={onRefresh}
+          colors={[colors.primary, 'red', 'orange', 'green']}
+        />
+      }
+      style={[globalStyles.mainContainer, globalStyles.globalMargin]}
+    >
+      <Title safe text="Pull to refresh"/>
+    </ScrollView>
+  );
+};
